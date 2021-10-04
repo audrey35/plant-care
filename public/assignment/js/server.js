9101dcbe1bf56dacfca762013151d0dc1bb92e9b
Audrey Jo <audreyjo@login-students.ccs.neu.edu>
2021-10-04 13:46:32 +0000

    set up the server

--- public/assignment/js/server.js
+++ public/assignment/js/server.js
@@ -1,8 +1,9 @@
+path = require('path');
 const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../../assignment')));
 
 app.listen(3000, function() {
 	console.log('App started on port 3000');
