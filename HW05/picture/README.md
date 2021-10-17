2ac32449cb361c432fafda150ecc5bc9d60ca7a6
Audrey Jo <audreyjo@login-students.ccs.neu.edu>
2021-10-18 01:11:54 +0900

    create picture of the day page

--- HW05/picture/README.md
+++ HW05/picture/README.md
@@ -29,13 +29,26 @@ git push a5 master:main // push the changes to heroku
 
 ## Create Home Page
 
-- create a form for submitting a date
-- get express to render the home page
+- created a form for submitting a date
+- got express to render the home page
   - kept rendering the default jade file
   - tried to setup hbs in app.js, but got
     _Error: No default engine was specified and no extension was provided._
     - https://stackoverflow.com/a/23596000
     - went back to L5-files to correctly setup hbs in app.js
+
+## Create Picture of the Day Page
+
+- tested the picture of the day API using the browser
+- the structure of the returned JSON was straightforward
+- created an html file with {{JSON object key}} for all keys that I wanted to use
+- copied the L6-files/requests/getRequestWithGet.js to my app.js to fetch data from the API
+- tried to render picture.html file, but failed
+  - `Error: Failed to lookup view "error" in views directory "cs5610_audreyjo/HW05/picture/views"`
+  - resolved the error by adding `const https = require('https');`
+- choosing a future date like October 28, 2021 creates an error
+  - https://stackoverflow.com/a/7086621
+  - caught the error and returned a message
 
 ## Website Screenshots
 
@@ -43,3 +56,7 @@ git push a5 master:main // push the changes to heroku
   ![picture of the home page](./public/images/index-screenshot.png)
 - use the date picker to choose a date (by default, today's date is highlighted)
   ![picture of the home page with date picker](./public/images/date-picker.png)
+- picture page
+  ![picture of the picture page](./public/images/picture-screenshot.png)
+- error page returned when user chooses a future date
+  ![picture of error page when a future date is chosen](./public/images/future-date-error.png)
--- HW05/picture/app.js
+++ HW05/picture/app.js
@@ -1,52 +1,96 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 
 // for res.render to render .html rather than default .jade
 // handles Error: No default engine was specified and no extension was provided.
 // https://stackoverflow.com/a/23596000
-var hbs = require('hbs');
-app.set('view engine', 'html');
-app.engine('html', hbs.__express);
+var hbs = require("hbs");
+app.set("view engine", "html");
+app.engine("html", hbs.__express);
 
+// for retrieving API data
+const https = require("https");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.post("/picture", (req, res) => {
+  var date = req.body.date;
+  var url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
+  var key = "?key=d2143e94-412a-448b-bb9d-51702131b42f";
+  pre =
+    "https://api.nasa.gov/planetary/apod?api_key=QE7r1w0Sdy3YDH8wkH3Fo7d3iZ1Ls6Mifbq7Hjzu";
+  url = pre + "&date=" + date;
+
+  request = https.get(url, (result) => {
+    if (result.statusCode !== 200) {
+      console.error(
+        `Did not get an OK from the server. Code: ${result.statusCode}`
+      );
+      //res.sendStatus(400)
+      res
+        .status(400)
+        .send(
+          `Bad Request. No picture available for the selected date ${date}.`
+        );
+    }
+
+    let data = "";
+
+    result.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    result.on("close", () => {
+      data = JSON.parse(data);
+      res.render("picture", {
+        copyright: data["copyright"],
+        date: data["date"],
+        explanation: data["explanation"],
+        image: data["url"],
+        title: data["title"],
+      });
+    });
+  });
+
+  request.on("error", (err) => {
+    console.error(
+      `Encountered an error trying to make a request: ${err.message}`
+    );
+    //res.sendStatus(400)
+    res
+      .status(400)
+      .send(`Bad Request. No picture available for the selected date ${date}.`);
+  });
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
