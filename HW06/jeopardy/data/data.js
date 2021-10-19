2905327b9558b86fb93760663f8ef35a0433571e
Audrey Jo <audreyjo35@gmail.com>
2021-10-19 22:00:35 +0000

    reduce jeopardy data size

--- HW06/jeopardy/data/data.js
+++ HW06/jeopardy/data/data.js
@@ -1,6 +1,7 @@
 /*
 Add JSON data to a MongoDB database called jeopardy in a collection called questions.
 JSON data comes from: https://domohelp.domo.com/hc/en-us/articles/360043931814-Fun-Sample-DataSets
+- reduced size to 6MB or 24683 records
 code source: 
 - how to insert data into MongoDB database: https://www.w3schools.com/nodejs/nodejs_mongodb_insert.asp
 - how to import JSON data into NodeJS: https://stackoverflow.com/a/7165572
